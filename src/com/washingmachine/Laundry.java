ef545d8f6f5a62948c4df1f275eaa8c7b29cddee
ssercanay <ssercanay.@gmail.com>
2020-10-21 20:11:46 +0200

    separating ui

--- src/com/washingmachine/Laundry.java
+++ src/com/washingmachine/Laundry.java
@@ -1,30 +1,21 @@
-package com.washingMachine;
+package com.washingmachine;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class UserInterface {
+public class Laundry {
   private final List<Machine> machines;
   private final Scanner scanner;
   private double currentTime;
-  
-  /**
-   * This class is ui for washing machines.
-   */
-  public UserInterface() {
+
+  public Laundry() {
     this.machines = new ArrayList<>();
     this.scanner = new Scanner(System.in);
     this.currentTime = 0;
   }
 
-  public void start() {
-    defineMachines();
-    welcoming();
-
-  }
-
-  private void defineMachines() {
+  public void defineMachines() {
     int programTime = 0;
     for (int i = 1; i < 4; i++) {
       String machineName = "machine " + i;
@@ -33,32 +24,7 @@ public class UserInterface {
     }
   }
 
-  private void welcoming() {
-    System.out.println("Welcome to laundry room"
-            + "\nWhat would you like to do?");
-
-    String menu = "[0] - Exit the room\n"
-            + "[1] - Check available machines" + "\n"
-            + "[2] - Abort machine" + "\n"
-            + "[3] - Price list" + "\n"
-            + "[4] - Earning of the day" + "\n"
-            + "[2] - List all machines";
-    while (true) {
-      System.out.println(menu);
-      System.out.print("Please enter an operation: ");
-      int operation = scanner.nextInt();
-      if (operation == 0) {
-        break;
-      } else if (operation == 1) {
-        setProgram();
-      } else {
-        System.out.println("Please enter a valid operation");
-      }
-
-    }
-  }
-
-  private void setProgram() {
+  public void setProgram() {
     remainedTime();
     if (isThereAvailableMachine()) {
       System.out.println("Choose a machine please!");
@@ -93,20 +59,4 @@ public class UserInterface {
     }
   }
 }
-/*
-    public void remainedTime(){
-      double remained = System.currentTimeMillis();
-      for (Machine machine:machines) {
-        remained = remained + machine.getTime()*60000;
-        while ((remained- System.currentTimeMillis()) != 0){
-          machine.setTime((int)((remained- System.currentTimeMillis())/60000));
-
-        }
-      }
-
-
-
-    }*/
-
-
 
