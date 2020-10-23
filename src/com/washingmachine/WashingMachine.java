b23ce98796a6af45452eb9e2bb458307f4ebb8a4
ssercanay <ssercanay.@gmail.com>
2020-10-23 11:13:21 +0200

    separating ui

---
+++ src/com/washingmachine/WashingMachine.java
@@ -0,0 +1,17 @@
+package com.washingmachine;
+
+import java.util.Timer;
+
+public class WashingMachine {
+  static int interval;
+  static Timer timer;
+  public static void main(String[] args) {
+    //program should have following functionalities
+    //is it occupied or not
+    //how many minutes left
+    //user should able to add time
+    UserInterface userInterface = new UserInterface();
+    userInterface.start();
+  }
+}
+
