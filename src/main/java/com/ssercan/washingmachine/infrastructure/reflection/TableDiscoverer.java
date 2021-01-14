361cbad1e442a915f9cfaefd08c72e63bb1ee6c0
ssercanay <ssercanay.@gmail.com>
2021-01-14 20:05:20 +0100

    joining-the-discoverer-classes

--- src/main/java/com/ssercan/washingmachine/infrastructure/reflection/TableDiscoverer.java
+++ src/main/java/com/ssercan/washingmachine/infrastructure/reflection/TableDiscoverer.java
@@ -1,14 +1,15 @@
 package com.ssercan.washingmachine.infrastructure.reflection;
 
 import org.reflections.Reflections;
+import java.lang.reflect.Field;
 import java.util.Set;
 
 public class TableDiscoverer{
+  private final Reflections reflections = new Reflections("com.ssercan.washingmachine.domain.machine");
+  private final Set<Class<?>> tableNotatedClasses = reflections.getTypesAnnotatedWith(Table.class);;
 
-  public void discover() {
 
-    Reflections reflections = new Reflections("com.ssercan.washingmachine.domain.machine");
-    Set<Class<?>> tableNotatedClasses = reflections.getTypesAnnotatedWith(Table.class);
+  public void discoverTable() {
 
     for (Class<?> klass: tableNotatedClasses) {
       Table getAnnotation = klass.getDeclaredAnnotation(Table.class);
@@ -16,4 +17,27 @@ public class TableDiscoverer{
     }
 
   }
+
+  public void discoverColumn() {
+    System.out.println("Columns = ");
+    for (Class<?> klass: tableNotatedClasses) {
+      for (Field field: klass.getDeclaredFields()) {
+        if (field.isAnnotationPresent(Column.class)) {
+          Column getAnnotation = field.getDeclaredAnnotation(Column.class);
+          System.out.println("  - " + field.getName() + " => " + "unique = " + getAnnotation.unique() +
+                  " nullable = " + getAnnotation.nullable());
+        }
+      }
+    }
+  }
+
+  public void discoverId() {
+    for (Class<?> klass : tableNotatedClasses) {
+      for (Field field : klass.getDeclaredFields()) {
+        if (field.isAnnotationPresent(Id.class)) {
+          System.out.println("Id = [" + field.getAnnotatedType() + "]");
+        }
+      }
+    }
+    }
 }
